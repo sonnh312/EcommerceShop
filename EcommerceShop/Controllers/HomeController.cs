e08d59a83923d323f6bf83f7d89cadbb51bcf9e2
sonnh312 <146912572+sonnh312@users.noreply.github.com>
2024-06-09 00:52:50 +0700

    first commit

---
+++ EcommerceShop/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using EcommerceShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EcommerceShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
